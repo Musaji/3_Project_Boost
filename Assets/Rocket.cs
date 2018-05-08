135085825b11389a80c488704b5b556c09ea3b74
Musaji <39049820+Musaji@users.noreply.github.com>
2018-05-07 17:26:06 -0700

    Preliminary Movement Scripts
    Added basic input output processing script to the ship(player) object

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Boosters Engaged");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
